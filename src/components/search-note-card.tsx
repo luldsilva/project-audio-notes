6eacf627c29cec039cf8075a0509f4c3b665d98d
Lucas Silva <lucasstrlisilva@outlook.com>
2024-02-06 20:21:30 -0300

    first commit

---
+++ src/components/search-note-card.tsx
@@ -0,0 +1,11 @@
+export function SearchNoteCard(){
+    return (
+        <form className="w-full">
+            <input
+                type="text"
+                placeholder="Busque em suas notas..."
+                className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+            />
+        </form>
+    )
+}
