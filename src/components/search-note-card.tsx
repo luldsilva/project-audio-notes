947db7f15cbb8a9d0110f32220175f4e06e7fbcd
Lucas Silva <lucasstrlisilva@outlook.com>
2024-02-08 18:16:45 -0300

    add feature for record voice and delete card

--- src/components/search-note-card.tsx
+++ src/components/search-note-card.tsx
@@ -1,10 +1,18 @@
-export function SearchNoteCard(){
+import { ChangeEvent, useState } from "react"
+
+interface HandleSearchProps{
+    handleSearch: (content: ChangeEvent<HTMLInputElement>) => void 
+  }
+
+export function SearchNoteCard({handleSearch}: HandleSearchProps){
+
     return (
         <form className="w-full">
             <input
                 type="text"
                 placeholder="Busque em suas notas..."
                 className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+                onChange={handleSearch}
             />
         </form>
     )
